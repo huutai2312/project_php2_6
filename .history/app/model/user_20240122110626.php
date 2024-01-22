621d48181f561e48490993eca5858bb66b6c5847
NHT <huutai90909@gmal.com>
2024-01-22 11:48:28 +0700

    Finish edit user

---
+++ .history/app/model/user_20240122110626.php
@@ -0,0 +1,96 @@
+<?php
+namespace App\model;
+use PDO;
+us
+
+class User
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new database();
+    }
+
+    private function getConnection()
+    {
+        return $this->db->connection_database();
+    }
+
+    public function getUserByEmail($email)
+    {
+        $conn = $this->getConnection();
+
+        $query = "SELECT * FROM ps_user WHERE email = :email";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getRoleAdmin($is_admin)
+    {
+        $conn = $this->getConnection();
+
+        $query = "SELECT * FROM ps_user WHERE is_admin = :is_admin";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':is_admin', $is_admin, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function registerUser($name, $email, $password, $date_created)
+    {
+        $conn = $this->getConnection();
+
+        $query = "INSERT INTO ps_user (name, email, password, address, phone, is_admin, date_created) VALUES (:name, :email, :password, :address, :phone, :is_admin, :date_created)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->bindValue(':address', '', PDO::PARAM_STR);
+        $stmt->bindValue(':phone', '0', PDO::PARAM_STR);
+        $stmt->bindValue(':is_admin', '0', PDO::PARAM_STR);
+        $stmt->bindParam(':date_created', $date_created, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        header("Location: /?registerSuccess=1"); 
+        exit();
+    }
+    
+    public function getAllUsersDesc()
+    {
+        $conn = $this->getConnection();
+        $query = "SELECT * FROM ps_user ORDER BY id DESC";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+    
+    public function adminUpdateProduct($id, $name, $price, $quantity, $image, $shortDesc, $longDesc)
+    {
+        $conn = $this->getConnection();
+        $query = "UPDATE ps_products 
+              SET name = :name, price = :price, quantity = :quantity, image = :image, short_desc = :short_desc, long_desc = :long_desc
+              WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->bindValue(':price', $price, PDO::PARAM_STR);
+        $stmt->bindValue(':quantity', $quantity, PDO::PARAM_INT);
+        $stmt->bindValue(':image', $image, PDO::PARAM_STR);
+        $stmt->bindValue(':short_desc', $shortDesc, PDO::PARAM_STR);
+        $stmt->bindValue(':long_desc', $longDesc, PDO::PARAM_STR);
+
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            // echo "Lỗi: " . $e->getMessage();
+            throw $e;
+        }
+    }
+}
---
+++ .history/index_20240122104531.php
@@ -0,0 +1,54 @@
+<?php
+require_once(__DIR__ . '/public/router.php');
+require_once(__DIR__ . '/app/controller/index.php');
+require_once realpath("vendor/autoload.php");
+
+use App\controller\Controller;
+
+$router = new Router();
+$router
+    //==========================================================================
+    //================================CLIENT====================================
+    //==========================================================================
+    ->get('/', [Controller::class, 'index'])
+    ->get('/sendmail', [Controller::class, 'sendMail'])
+    ->get('/cua-hang', [Controller::class, 'cuahang'])
+    ->get('/san-pham', [Controller::class, 'productDetail'])
+    ->get('/tai-khoan', [Controller::class, 'taikhoan'])
+    ->get('/login', [Controller::class, 'login'])
+    ->post('/loginUser', [Controller::class, 'loginUser'])
+    ->get('/register', [Controller::class, 'register'])
+    ->post('/register', [Controller::class, 'registerUser'])
+    ->get('/logout', [Controller::class, 'logout'])
+    ->post('/cart', [Controller::class, 'cart'])
+    ->get('/cart', [Controller::class, 'cart'])
+    ->get('/checkout', [Controller::class, 'checkout'])
+    ->post('/checkout', [Controller::class, 'userOrder'])
+    //==========================================================================
+    //=================================ADMIN====================================
+    //==========================================================================
+    ->get('/admin', [Controller::class, 'adminIndex'])
+    //+Product
+    ->get('/admin/products', [Controller::class, 'adminProducts'])
+    ->get('/admin/add_product', [Controller::class, 'adminAddProduct'])
+    ->post('/admin/add_product', [Controller::class, 'adminAddProduct'])
+    ->get('/admin/edit_product', [Controller::class, 'adminEditProduct'])
+    ->get('/admin/update_product', [Controller::class, 'adminUpdateProduct'])
+    ->post('/admin/update_product', [Controller::class, 'adminUpdateProduct'])
+    ->get('/admin/delete_product', [Controller::class, 'adminDeleteProduct'])
+    //+User
+    ->get('/admin/users', [Controller::class, 'adminUsers'])
+    ->get('/admin/add_user', [Controller::class, 'adminAddProduct'])
+    ->post('/admin/add_user', [Controller::class, 'adminAddProduct'])
+    ->get('/admin/edit_user', [Controller::class, 'adminEditProduct'])
+    ->get('/admin/update_user', [Controller::class, 'adminUpdateProduct'])
+    ->post('/admin/update_user', [Controller::class, 'adminUpdateProduct'])
+    ->get('/admin/delete_user', [Controller::class, 'adminDeleteProduct'])
+    //+Order
+    ->get('/admin/orders', [Controller::class, 'adminOrders']);
+
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'],
+    strtolower($_SERVER['REQUEST_METHOD']),
+    isset($_GET['id']) ? $_GET['id'] : null
+);
